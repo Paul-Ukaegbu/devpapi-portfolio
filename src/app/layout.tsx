965d0210c683eac439b88dccc694c26ee23fae30
DevPapi <paulukaegbu@devpapi.io>
2024-03-02 04:00:49 -0600

    My app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -11,12 +12,24 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Mr+Dafoe&family=Splash&display=swap'),
+      </style>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Mr+Dafoe&family=Rochester&display=swap')
+      </style>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap')
+      </style>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Libre+Barcode+128+Text&family=VT323&display=swap')
+      </style>
     </html>
   );
 }
---
+++ src/app/lib/placeholder.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        name: 'Paul Ukaegbu',
+        email: 'paulukaegbu@devpapi.io'
+    },
+]
+
+const projects = [
+    {
+        id: 1,
+        name: 'DevPapi',
+        description: 'A platform for developers to showcase their projects'
+    }
+]
